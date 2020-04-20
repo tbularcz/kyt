7f7a6d0ff1d0ca3bb3b353c4d064a3daeb446c70
Bularczyk <bularct@emea.corpdir.net>
2020-04-20 17:16:51 +0200

    First Grid and Navigation

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Appbar from './components/Appbar.js';
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Appbar />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
